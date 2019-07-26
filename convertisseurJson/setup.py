b244c04be7576ef91d28f4c3c19dc10334c26c3d
Eric <travers.eric@gmail.com>
2019-07-26 15:17:37 +0200

    Update setup.py

--- convertisseurJson/setup.py
+++ convertisseurJson/setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Yaml convertisseur for files",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/JoHn3r/python-py/tree/master/convertisseurJson",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
