562927264b7bc623c19dd5706b2361b924249a2d
Eric <travers.eric@gmail.com>
2019-09-08 00:16:56 +0200

    Update createCSR.py

--- createCSR.py
+++ createCSR.py
@@ -9,7 +9,14 @@ for i in mydata:
     with open('template.txt') as template, open(i["FQDN"]+'.cfg',"w+") as output:
         for line in template:
             for key, value in i.items():
-                line = line.replace(key, str(value))
+                if key == "ALT_NAMES":
+                    l = []
+                    for v in value:
+                        l.append("DNS:" + v)
+                        final_list = ",".join(l)
+                    line = line.replace(key, final_list)
+                else:
+                    line = line.replace(key, value)
             output.write(line)
 
           
