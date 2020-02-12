208ac06bf1a34f4a3a1811cb1313bb9be1ea8d19
Eric TRAVERS <eric.travers@seloger.com>
2020-02-12 17:57:19 +0100

    changes

--- simpleAPI/getChild.ps1
+++
@@ -1,9 +0,0 @@
-function getDir {
-    # param (
-    #     OptionalParameters
-    # )
-    $res=get-childitem "C:\Users\eric.travers.POLIRIS\" | ?{ $_.PSIsContainer } | Select-Object -exp Name
-    return ,$res
-}
-
-
--- simpleAPI/main.py
+++ simpleAPI/main.py
@@ -1,20 +1,37 @@
 import subprocess
 import os
 import json
-from flask import abort
-from flask import Flask
-from flask import request
-from flask import Response
+import requests
+import dominate
+from datetime import datetime
+from flask import abort, Flask, request, Response, render_template
 
 app = Flask(__name__)
 
 @app.route("/", methods = ['GET'])
 def getInf():
-    # if not request.json:
-    #     abort(400)
-    pow_res=subprocess.Popen(['powershell','Get-ChildItem | ?{ $_.PSIsContainer } | Select-Object -exp Name'],stdout=subprocess.PIPE,shell=True)
-    js=json.dumps(str(pow_res.communicate()))
-    resp=Response(js, status=200, mimetype='application/json')
-    return resp
+    f= open("./templates/main.htm","w+")
+    for i in range(1,6):
+        r = requests.get(f'http://search-list-rct-{i}.seloger.com/healthcheck')
+        if r.json() == 'OK':
+            f.write(f'<BODY><H2 style="color:green;">RCT{i} : OK</H1>\n')
+        else:
+            f.write(f'<BODY><H2 style="color:red;">RCT{i} : KO</H1>\n')
+    now = datetime.now()
+    f.write(f'<BODY><H3>{now}</H3>\n<meta http-equiv="refresh" content="30">')
+    f.close()
+    return render_template('main.htm')
+
+@app.route("/repos", methods = ['GET','POST'])
+def repos():
+    f= open("./templates/repos.htm","w+")
+    for i in range(1,6):
+        r = requests.get(f'http://search-list-rct-{i}.seloger.com/healthcheck')
+        if r.json() == 'OK':
+            f.write(f'<BODY><H2 style="color:green;">RCT{i} : OK</H1>\n')
+    f.write('<meta http-equiv="refresh" content="30">')
+    f.close()
+    return render_template('main.htm')
+
 if __name__ == "__main__":
     app.run()
